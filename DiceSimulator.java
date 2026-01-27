9dd71f4b546268e914b4cf58fe947792e6b86ddb
sagunadk7 <sagunadhikari100@gmail.com>
2026-01-27 21:19:24 +0545

    Added computer vs player feature

--- DiceSimulator.java
+++ DiceSimulator.java
@@ -5,16 +5,32 @@ public class DiceSimulator {
         int randomValue = random.nextInt(6)+1;
         return randomValue;
     } 
+    static String checkWinner(int userWin,int computerWin) {
+        return (userWin>computerWin)?"Congratulation!! You Won!!" : "You loose!!";
+    }
     public static void main(String[] args) {
             int [] count = new int [6];
-            for(int i = 0; i< 100; i++) {
-                int roll = rollDice();
-                count[roll-1]++;
+
+            int round = 5;
+            int userWin = 0;
+            int computerWin=0;
+            for(int i = 0; i<=round; i++) {
+                int userRoll = rollDice();
+                int computerRoll = rollDice();
+                if(userRoll>computerRoll) {
+                    userWin++;
+                } else if(computerRoll>userRoll) {
+                    computerWin++;
+                }
+                count[userRoll-1]++;
+                count[computerRoll-1]++;
             }
-            for(int i = 0; i<=5;i++) {
+            for(int i = 0; i<=round;i++) {
                 double probability = (count[i]/100.0) *100; 
                System.out.printf("%d appeared %d times Probability: %.2f%%%n",i + 1, count[i], probability);
 
             }
+
+            System.out.println(checkWinner(userWin, computerWin));
     }
 }
